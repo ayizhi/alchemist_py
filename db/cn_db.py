b6a56584a7020d89320c3a45d80dc257d50775b7
ayizhi <zhangyiizhi@126.com>
2017-03-23 00:10:16 +0800

    fix

--- db/cn_db.py
+++ db/cn_db.py
@@ -1,1 +1,17 @@
-print 'may the force be with you'
+print '================== may the force be with you ================='
+import pymongo
+from pprint import pprint
+from pymongo import MongoClient
+import tushare
+
+
+class Db():
+	def __init__(self):
+		self.client = MongoClient('localhost',27017);
+		self.db = client.ticker_master
+
+	def save_ticker_names_into_db():
+		collection = db.symbol
+
+
+		print 111
