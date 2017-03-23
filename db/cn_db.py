15be349778e617fb6fd3f96b67517608c353e021
张奕智 <zhangyizhi@qijiayoudao.com>
2017-03-23 21:46:49 +0800

    fix

--- db/cn_db.py
+++ db/cn_db.py
@@ -2,14 +2,16 @@ print '================== may the force be with you ================='
 import pymongo
 from pprint import pprint
 from pymongo import MongoClient
-import tushare
-
+import tushare as ts
+import json
 
 class Database():
 	def __init__(self):
 		self.client = MongoClient('localhost',27017);
-		self.db = client.ticker_master
+		self.db = self.client.ticker_master
 
-	def save_ticker_names_into_db():
-		collection = db.symbol
-		print 111
+	def save_ticker_names_into_db(self):
+		collection = self.db.symbol
+		ticker_list = ts.get_stock_basics()
+		ticker_json = json.loads(ticker_list.to_json(orient='records'))
+		collection.insert(ticker_json)
--- strategy/trader.py
+++ strategy/trader.py
@@ -1,11 +1,13 @@
 #coding: utf-8
 import sys
 sys.path.append('..')
-import db.cn_db as database
-from db.cn_db import Database as Db
+from db.cn_db import Database
 from pprint import pprint
 import numpy as np
 import pandas as pd
 import datetime
 
-pprint(Db)
+
+if __name__ == '__main__' :
+    Db = Database();
+    Db .save_ticker_names_into_db()
