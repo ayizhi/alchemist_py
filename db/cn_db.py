4e96f0e969e171cc93d3135742e03edbcc49a201
张奕智 <zhangyiizhi@126.com>
2017-03-23 20:37:49 +0800

    fix

--- db/cn_db.py
+++ db/cn_db.py
@@ -5,13 +5,11 @@ from pymongo import MongoClient
 import tushare
 
 
-class Db():
+class Database():
 	def __init__(self):
 		self.client = MongoClient('localhost',27017);
 		self.db = client.ticker_master
 
 	def save_ticker_names_into_db():
 		collection = db.symbol
-
-
 		print 111
--- strategy/trader.py
+++ strategy/trader.py
@@ -1,11 +1,11 @@
 #coding: utf-8
 import sys
 sys.path.append('..')
-import db.cn_db as db
-import db.Db as Db
+import db.cn_db as database
+from db.cn_db import Database as Db
 from pprint import pprint
 import numpy as np
 import pandas as pd
 import datetime
 
-pprint(db.Db)
+pprint(Db)
