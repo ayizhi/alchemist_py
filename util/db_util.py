49de3d9914989fdf7b483413542cd724eee95a91
张奕智 <zhangyiizhi@126.com>
2017-04-16 22:41:55 +0800

    fix db

--- util/db_util.py
+++ util/db_util.py
@@ -15,7 +15,7 @@ class DbUtil(Database):
 		for i in range(len(ticker_list)):
 			pbar.update(i+1)
 			time.sleep(0.0001)
-			
+
 			ticker = ticker_list[i]
 			ticker_id = ticker['code']
 			ticker_name = ticker['name']
@@ -32,7 +32,3 @@ class DbUtil(Database):
 
 
 
-
-dbUtil = DbUtil()
-
-dbUtil.get_middle_33_average_volume_tickers()
