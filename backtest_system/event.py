21810f1a84c9871be5fd38891752a093f6e7a8aa
张奕智 <zhangyiizhi@126.com>
2017-05-01 10:48:21 +0800

    fix

--- backtest_system/event.py
+++ backtest_system/event.py
@@ -22,13 +22,6 @@ class SignalEvent(Event):
 
 class OrderEvent(Event):
 	def __init__(self,symbol,order_type,quantity,direction):
-	"""
-	Parameters:
-        symbol - The instrument to trade.
-        order_type - 'MKT' or 'LMT' for Market or Limit.
-        quantity - Non-negative integer for quantity.
-        direction - 'BUY' or 'SELL' for long or short.
-    """
 		self.type = "ORDER"
 		self.symbol	= symbol
 		self.order_type  = order_type
@@ -39,35 +32,36 @@ class OrderEvent(Event):
 		print ("Order: Symbol=%s, Type=%s, Quantity=%s,Direction=%s") % (self.symbol,self.order_type,self.quantity,self.direction)
 
 class FillEvent(Event):
-	def __init__(self,timeindex,symbol,exchange,quantity,direction,fill_cost,commisson=None):
-			"""
-	Parameters:
-        timeindex - The bar-resolution when the order was filled.
-        symbol - The instrument which was filled.
-        exchange - The exchange where the order was filled.
-        quantity - The filled quantity.
-        direction - The direction of fill ('BUY' or 'SELL')
-        fill_cost - The holdings value in dollars.
-        commission - An optional commission sent from IB.
-    """
-   		self.type = "FILL"
-   		self.timeindex = timeindex
-   		self.symbol = symbol
-   		self.exchange = exchange
-   		self.quantity = quantity
-   		self.direction = direction
-   		self.fill_cost = fill_cost
+    def __init__(self,timeindex,symbol,exchange,quantity,direction,fill_cost,commisson=None):
+        """
+        Parameters:
+            timeindex - The bar-resolution when the order was filled.
+            symbol - The instrument which was filled.
+            exchange - The exchange where the order was filled.
+            quantity - The filled quantity.
+            direction - The direction of fill ('BUY' or 'SELL')
+            fill_cost - The holdings value in dollars.
+            commission - An optional commission sent from IB.
+        """
+        self.type = "FILL"
+        self.timeindex = timeindex
+        self.symbol = symbol
+        self.exchange = exchange
+        self.quantity = quantity
+        self.direction = direction
+        self.fill_cost = fill_cost
 
-   		#calculate the commission
-   		if commisson is None:
-   			self.commisson = self.calculate_ib_commission()
-   		else self.commisson = commisson
+        #calculate the commission
+        if commisson is None:
+          self.commisson = self.calculate_ib_commission()
+        else:
+          self.commisson = commisson
 
-   	def calculate_ib_commission(self):
-   		full_cost = 1.3
-   		if self.quantity <= 500:
-   			full_cost = max(1.3, 0.013 * self.quantity)
-   		else:
-   			full_cost = max(1.3,0.013 * self.quantity)
+    def calculate_ib_commission(self):
+        full_cost = 1.3
+        if self.quantity <= 500:
+          full_cost = max(1.3, 0.013 * self.quantity)
+        else:
+          full_cost = max(1.3,0.013 * self.quantity)
 
-   		return full_cost
+        return full_cost
--- backtest_system/execution.py
+++ backtest_system/execution.py
@@ -2,6 +2,10 @@
 
 from abc import ABCMeta,abstractmethod
 import datetime
+from ib.ext.Contract import Contract
+from ib.ext.Order import Order
+from ib.opt import ibConnection, message
+
 try:
 	import Queue as queue
 except ImportError:
@@ -19,10 +23,10 @@ class ExecutionHandler(object):
 
 class SimulatedExecutionHandler(ExecutionHandler):
 	def __init__(self,events):
-		 """
-        Parameters:
-        events - The Queue of Event objects.
-        """
+		"""
+		Parameters:
+		events - The Queue of Event objects.
+		"""
 		self.events = events
 
 	def execute_order(self,event):
@@ -44,5 +48,94 @@ class IBExecutionHandler(ExecutionHandler):
 
 
 	def _error_handler(self, msg):
-		if msg.typeName == "openOrder" and msg.orderId == self.order_id and not self.fill_dict.has_key(msg.orderId):
+		print("Server Error: %s" % msg)
+
+
+	def _reply_handler(self,msg):
+		#Handler open order orderId processing
+		if msg.typeName == "openOrder" and msg.orderId == msg.orderId and not self.fill_dict.has_key(msg.orderId):
 			self.create_fill_dict_entry(msg)
+
+		#Handle Fills
+		if msg.typeName == "orderStatus" and msg.status == "Filled" and self.fill_dict[msg.orderId]['filled'] == False:
+			self.create_fill(msg)
+
+		print("Server Response: %s , %s\n" % (msg.typeName,msg))
+
+	def create_tws_connection(self):
+		"""
+        Connect to the Trader Workstation (TWS) running on the
+        usual port of 7496, with a clientId of 100.
+        The clientId is chosen by us and we will need
+        separate IDs for both the execution connection and
+        market data connection, if the latter is used elsewhere.
+        """
+		tws_conn = ibConnection()
+		tws_conn.connect()
+		return tws_conn
+
+	def create_initial_order_id(self):
+		# There is scope for more logic here, but we
+        # will use "1" as the default for now.
+		return 1
+
+	def register_handlers(self):
+		self.tws_conn.register(self._error_handler, "Error")
+		self.tws_conn.registerAll(self._reply_handler)
+
+	def create_contract(self, symbol, sec_type, exch, prim_exch, curr):
+		contract = Contract()
+		contract.m_symbol = symbol
+		contract.m_secType = sec_type
+		contract.m_exchange = exch
+		contract.m_primaryExch = prim_exch
+		contract.m_currency = curr
+		return contract
+
+	def create_order(self, order_type, quantity, action):
+		order = Order()
+		order.m_orderType = order_type
+		order.m_totalQuantity = quantity
+		order.m_action = action
+		return order
+
+	def create_fill_dict_entry(self,msg):
+		self.fill_dict[msg.orderId] = {
+			'symbol': msg.contract.m_symbol,
+			'exchange': msg.contract.m_exchange,
+			'direction': msg.order.m_action,
+			'filled': False
+		}
+
+	def create_fill(self,msg):
+		fd = self.fill_dict[msg.orderId]
+
+		symbol = fd['symbol']
+		exchange = fd['exchange']
+		filled = msg.filled
+		direction = fd['direction']
+		fill_cost = msg.avgFillprice
+
+		fill = FillEvent(
+			datetime.datetime.utcnow(), symbol, exchange, filled, direction, fill_cost
+			)
+		self.fill_dict[msg.orderId]['filled'] = True
+		self.events.put(fill_event)
+
+	def execute_order(self,event):
+		if event.type == 'ORDER':
+			asset = event.symbol
+			asset_type = "STK"
+			order_type = event.order_type
+			quantity = event.quantity
+			direction = event.direction
+
+			ib_contract = self.create_contract(asset, asset_type, self.order_routing, self.order_routing, self.currency)
+
+			ib_order = self.create_order(order_type,quantity, direction)
+
+			self.tws_conn.placeOrder(
+				self.order_id, ib_contract, ib_order)
+
+			time.sleep(1)
+			self.order_id += 1
