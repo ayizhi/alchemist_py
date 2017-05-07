308b6ada1a7344d42a7271f2db9fea6f65cb3c8f
张奕智 <zhangyiizhi@126.com>
2017-05-07 11:15:39 +0800

    stone

--- backtest_system/stone.py
+++ backtest_system/stone.py
@@ -1,4 +1,37 @@
-321#coding: utf-8
+#coding: utf-8
 #alchemist's stone
 
-from data import historicalDbDataHandler
+import datetime
+import numpy as np
+import pandas as pd
+
+from backtest import Backtest
+from data import HistoricalDbDataHandler
+from event import SignalEvent
+from execution import SimulatedExecutionHandler
+from portfolio import Portfolio
+from strategy import Strategy
+
+class MovingAverageCrossStrategy(Strategy):
+    def __init__(self,bars,events,short_window=100,long_window=100):
+        self.bars = bars
+        self.symbo_list = self.bars.symbo_list
+        self.events = events
+        self.short_window = short_window
+        self.long_window = long_window
+
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+        bought = {}
+        for s in self.symbo_list:
+            bought[s] = 'OUT'
+        return bought
+
+
+if __name__ = '__main__':
+    symbo_list = ['600533','000503']
+    initial_capital = 100000.0
+    start_date = datetime.datetime(2015,1,1,0,0,0)
+    heart_beat = 0.0
+
