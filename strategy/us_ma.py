100d28e0392d085217c71a1d830bd0f980780a54
张奕智 <zhangyiizhi@126.com>
2017-06-29 00:46:48 +0800

    moving average add linear regression done

--- strategy/us_ma.py
+++ strategy/us_ma.py
@@ -9,6 +9,8 @@ from strategy_base import Strategy
 import sklearn
 from sklearn import linear_model
 
+import matplotlib.pyplot as plt
+
 class MA_strategy(Strategy):
     def __init__(self,tickers):
         self.short_k_day = 5
@@ -42,27 +44,59 @@ class MA_strategy(Strategy):
                 profit_short_k = self.db.get_profit_by_days(ticker,self.short_k_day)
                 volume = ticker_data['volume'][-1]
 
+                # Plot outputs
+                # plt.scatter(train_x, train_y,  color='black')
+                # plt.plot(train_x, reg.predict(train_x), color='blue',linewidth=3)
 
-                reg = linear_model.LinearRegression()
-                print (short_k)
+                # plt.xticks(())
+                # plt.yticks(())
+
+                # plt.show()
+
 
 
                 #接下来还要从以下几点考虑
                 #成交量，20% － 50％，
                 #盈利前 10-30％
                 if(short_k_price > middle_k_price) and (middle_k_price > long_k_price) and abs(middle_k_price - long_k_price) < 1:
-                    self.ticker_filter_result.append({
-                        'ticker': ticker,
-                        'profit': profit_short_k,
-                        'volume': volume
-                        })
 
-            break
+                    #linear regression
+                    reg = linear_model.LinearRegression()
+                    train_short_x = np.arange(short_k.shape[0]).reshape(short_k.shape[0],1)
+                    train_short_y = np.array(short_k)
+                    train_middle_x = np.arange(middle_k.shape[0]).reshape(middle_k.shape[0],1)
+                    train_middle_y = np.array(middle_k)
+                    train_long_x = np.arange(long_k.shape[0]).reshape(long_k.shape[0],1)
+                    train_long_y = np.array(long_k)
+
+                    reg.fit(train_short_x,train_short_y)
+                    short_k_coef = reg.coef_
+                    short_k_intercept = reg.intercept_
+
+                    reg.fit(train_middle_x,train_middle_y)
+                    middle_k_coef = reg.coef_
+                    middle_k_intercept = reg.intercept_
+
+                    reg.fit(train_long_x,train_long_y)
+                    long_k_coef = reg.coef_
+                    long_k_intercept = reg.intercept_
+
+                    if short_k_coef > middle_k_coef and middle_k_coef > long_k_coef:
+
+                        self.ticker_filter_result.append({
+                            'ticker': ticker,
+                            'profit': profit_short_k,
+                            'volume': volume
+                            })
+
+
 
         #10% - 30% profit
-        self.ticker_filter_result = pd.DataFrame(self.ticker_filter_result).sort(['profit'])
+        self.ticker_filter_result = pd.DataFrame(self.ticker_filter_result).sort_values(by=['profit'])
+        print (self.ticker_filter_result)
+
         shape = self.ticker_filter_result.shape[0]
-        self.ticker_filter_result = self.ticker_filter_result[int(shape * 0.7): int(shape * 0.9)].sort(['volume'])
+        self.ticker_filter_result = self.ticker_filter_result[int(shape * 0.7): int(shape * 0.9)].sort_values(by=['volume'])
         #20%-50% volume
         shape = self.ticker_filter_result.shape[0]
         self.ticker_filter_result = self.ticker_filter_result[int(shape * 0.5): int(shape * 0.8)]
