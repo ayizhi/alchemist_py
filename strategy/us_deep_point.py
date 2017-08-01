7e4db350c66b3b9abe8aa9a2e0fc59201f7dd64f
张奕智 <zhangyiizhi@126.com>
2017-08-01 09:06:26 +0800

    con

--- strategy/us_deep_point.py
+++ strategy/us_deep_point.py
@@ -16,18 +16,23 @@ import statsmodels.tsa.stattools as ts
 
 class Deep_point_strategy(Strategy):
     def __init__(self):
-        self.date_range = 10
+        self.date_range = 15
         self.db = US_Database()
 
     def deal_data(self,symbol):
         #find the biggest drop around 10 days
         start_date = datetime.datetime.today() - datetime.timedelta(self.date_range)
-        ticker_data = self.db.get_ticker_by_id_not_consecutive_date(symbol,start_date=start_date)
+        ticker_data = self.db.get_ticker_by_id_not_consecutive_date(symbol,start_date=start_date).reset_index()
         ticker_data['delta'] = ticker_data['close'] - ticker_data['open']
         ticker_data['delta_pc'] = (ticker_data['close'] - ticker_data['open']) * 100 / ticker_data['open']
 
         if not ticker_data[ticker_data['delta_pc'] > 6].empty:
-            print (ticker_data)
+            deep_index = ticker_data[ticker_data['delta_pc'] > 6].index[-1]
+            if deep_index <= 2:
+                #volume is so few and the change of price after deep is slience
+                print(ticker_data.iloc[deep_index : ])
+
+
 
 
 
