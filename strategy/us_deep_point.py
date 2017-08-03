8934d773c0bc5634911136f3b1673f54aad4b1f1
ayizhi <zhangyiizhi@126.com>
2017-08-03 08:48:33 +0800

    opt continnue

--- strategy/us_deep_point.py
+++ strategy/us_deep_point.py
@@ -20,7 +20,7 @@ import seaborn as sns
 class Deep_point_strategy(Strategy):
     def __init__(self):
         self.date_range = 15
-        self.lr_range = 50
+        self.lr_range = 70
         self.db = US_Database()
 
     def deal_data(self,symbol):
@@ -38,11 +38,7 @@ class Deep_point_strategy(Strategy):
             X_train = np.array(pre_ticker_data.index)
             y_train = np.array(pre_ticker_data.close)
             xx = np.linspace(0,pre_ticker_data.index[-1],100)
-            df_train = pd.DataFrame({'x': X_train,'y': y_train})
-
-        
-            sns.jointplot('x','y',df_train[['x','y']],kind = 'scatter',color='red')
-
+            
             quadratic_featurizer = PolynomialFeatures(degree=2)
             X_train_quadratic = quadratic_featurizer.fit_transform(X_train.reshape(X_train.shape[0],1))
             regressor_quadratic = LinearRegression()
@@ -51,15 +47,25 @@ class Deep_point_strategy(Strategy):
             xx_quadratic = quadratic_featurizer.transform(xx.reshape(xx.shape[0], 1))
             yy_quadratic = regressor_quadratic.predict(xx_quadratic)
 
+            # y = ax2 + bx + c
+            coef_ = regressor_quadratic.coef_
+            a = coef_[-1]
+            b = coef_[1]
+            # 我们想让导数为零的点在今天附近或者之后,且二次函数a为正
+            # 导数为零点
+            d_p = (b * -1) / (2 * a)
+            d_range = len(X_train)
+            if a > 0 and (d_p > d_range - 10 and d_p < d_range + 10):
+                print (symbol, d_p, d_range)
+                df_predict_quadratic = pd.DataFrame({'x': xx, 'y': yy_quadratic})
+                sns.jointplot('x','y',df_predict_quadratic[['x','y']],kind = 'scatter',color='blue')
+                sns.plt.show()
 
-            df_predict_quadratic = pd.DataFrame({'x': xx, 'y': yy_quadratic})
-            sns.jointplot('x','y',df_predict_quadratic[['x','y']],kind = 'scatter',color='blue')
-            sns.plt.show()
-
-            deep_index = ticker_data[ticker_data['delta_pc'] > 6].index[-1]
-            if deep_index <= 2:
-                #volume is so few and the change of price after deep is slience
-                print(ticker_data.iloc[deep_index : ])
+                #判断有大跌
+                deep_index = ticker_data[ticker_data['delta_pc'] > 4].index[-1]
+                if deep_index <= 2:
+                    #volume is so few and the change of price after deep is slience
+                    print(ticker_data.iloc[deep_index : ])
 
 
 
@@ -72,7 +78,7 @@ if __name__ == '__main__':
     #loop
     # symbols.map(lambda x: dp.deal_data(x))
     for index in range(len(symbols)):
-        if index > 100 :
-            break
+        # if index > 100 :
+        #     break
         symbol = symbols[index]
         dp.deal_data(symbol)
